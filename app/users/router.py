26ca5d5ee837a5789bbe601a6d18da597aa75835
RomanK17 <romankopilov1712@gmail.com>
2024-07-16 17:39:12 +0300

    add 401 error

--- app/users/router.py
+++ app/users/router.py
@@ -8,7 +8,7 @@ router = APIRouter(prefix='/auth', tags=['Пользователи'])
 async def register_user(user_data: SchemaUserAuth):
     existing_user = await UserService.find_one_or_none(email=user_data.email) 
     if existing_user:
-        raise HTTPException(status_code=401, detail='Пользователь с таким email уже существует')
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Пользователь с таким email уже существует')
     hashed_password = get_password_hash(user_data.password)
     await UserService.insert_data(email=user_data.email, hashed_password=hashed_password)
     
@@ -17,6 +17,6 @@ async def auth_user(response: Response, user_data: SchemaUserAuth):
     existing_user = await authenticate_user(email=user_data.email, password=user_data.password)
     if not existing_user:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Пользователя с таким email или паролем не существует!')
-    access_token = create_access_token({'sub': existing_user.id})
+    access_token = create_access_token({'sub': str(existing_user.id)})
     response.set_cookie('booking_access_token', access_token, httponly=True)
     return access_token
