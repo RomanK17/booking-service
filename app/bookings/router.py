e459c448fe5c6d06911040467fcc004b92b04a8d
RomanK17 <romankopilov1712@gmail.com>
2024-07-16 17:40:07 +0300

    add filtration by user id in router get_bookings

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -1,11 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, Request
 from sqlalchemy import select
 
 from app.bookings.service import BookingServise
 from app.bookings.schemas import BookingsSchema
+from app.users.dependencies import get_current_user
 
 router = APIRouter(prefix='/bookings', tags=['Bookings'])
 
 @router.get('')
-async def get_bookings() -> list[BookingsSchema]:
-    return await BookingServise.find_all()
+async def get_bookings(user_id: int = Depends(get_current_user)) -> list[BookingsSchema]:
+    
+    return await BookingServise.find_all(user_id=user_id)
