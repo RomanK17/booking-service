1cf6d80eb29ed6b66db8ae0b40eaf901205a74c0
RomanK17 <romankopilov1712@gmail.com>
2024-07-16 18:45:16 +0300

    rewrite exceptions

--- app/exceptions.py
+++ app/exceptions.py
@@ -1,5 +1,18 @@
 from fastapi import HTTPException, status
 
-UserAlredyExistsException = HTTPException(status_code=status.HTTP_409_CONFLICT, detail='Пользователь с такими данными уже есть в базе данных')
+class BookingException(HTTPException):
+    detail = ""
+    
+    def __init__(self):
+        super().__init__(status_code=self.status_code, detail=self.detail)
+        
 
-UserNotRegisteredException = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Пользователя с таким email или паролем не существует!')
+class UserAlredyExistsException(BookingException):
+    status_code=status.HTTP_409_CONFLICT
+    detail='Пользователь с такими данными уже есть в базе данных'
+    
+    
+class UserNotRegisteredException(BookingException):
+    status_code=status.HTTP_401_UNAUTHORIZED
+    detail='Пользователя с таким email или паролем не существует!'
+    
