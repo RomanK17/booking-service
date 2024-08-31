6e444f34fdfdce350640fe820e0fba4bab1bb0b7
RomanK17 <romankopilov1712@gmail.com>
2024-08-31 10:07:53 +0300

    dlt unused router

--- main.py
+++ main.py
@@ -13,31 +13,6 @@ app.include_router(user_router)
 app.include_router(booking_router)
 app.include_router(page_router)
 
-class SchemaHotel(BaseModel):
-    adress: str
-    name: str
-    start: int
-    
-
-class SearchHotelsArgs(BaseModel):
-    def __init__(self,
-    location: str,
-    date_from: date,
-    date_to: date,
-    has_spa: Optional[bool],
-    stars: Optional[int] = Query(None, ge=1, le=5) 
-    ):
-        self.location = location
-        self.date_from = date_from
-        self.date_to = date_to
-        self.has_spa = has_spa
-        self.stars = stars
-        
-    
-
-@app.get("/hotels")
-def get_hotels( search_args : SearchHotelsArgs = Depends()) -> list[SchemaHotel]:
-    return search_args
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
